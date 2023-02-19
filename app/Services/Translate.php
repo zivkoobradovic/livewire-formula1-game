491beef80f51c46c16f94bdf33aa2d1f646f35d8
Zivko <zozo@Zivkos-MacBook-Pro.local>
2023-02-19 10:58:01 +0100

    transpate

---
+++ app/Services/Translate.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Services;
+
+
+class Translate
+{
+    public static function getTranslation()
+    {
+        return  [
+            'eng' => [
+                'login' => 'login',
+                'leaderboard' => 'leaderboard',
+                'email' => 'email',
+                'username' => 'user name',
+                'phone' => 'phone (optional)',
+                'girl' => 'girl',
+                'boy' => 'boy',
+                'race' => 'Race',
+
+                'title_2' => 'Find Jeddah Cornish Circuit',
+                'drag_2' => 'drag here',
+
+                'title_3' => 'get the car ready for the race',
+
+                'title_4' => 'select the finish flag',
+                'drag_4' => 'select the finish flag',
+
+                'title_5' => 'jeddah cornish circuit has the most',
+                'traffic_5' => 'traffic',
+                'corners_5' => 'corners',
+                'stops_5' => 'stops',
+
+                'title_6' => 'who won the race in Azooz\'s dream?',
+
+                'score' => 'your score',
+                'share' => 'share',
+            ],
+            'arab' => [
+                'login' => 'الدخول',
+                'leaderboard' => 'المتصدرين',
+                'email' => 'البريد الإلكتروني ',
+                'username' => 'اسم المستخدم ',
+                'phone' => 'الجوال (اختياري )',
+                'girl' => 'بنت',
+                'boy' => 'ولد',
+                'race' => 'تسابق',
+
+                'title_2' => 'أوجد حلبة كورنيش جدة',
+                'drag_2' => 'اسحب هنا',
+
+                'title_3' => 'جهرة السيارة للسباق',
+
+                'title_4' => 'اختار علم نهاية السباق',
+                'drag_4' => 'اسحب هنا',
+
+                'title_5' => 'حلبة كورنيس جدة تتميز بكثرة',
+                'traffic_5' => 'الاشارات',
+                'corners_5' => 'المنعطفات',
+                'stops_5' => 'محطات الوقوف',
+
+                'title_6' => 'من فاز السباق في حلم عزوز',
+
+                'score' => 'النتيجة',
+                'share' => 'شارك',
+            ]
+        ];
+    }
+}
