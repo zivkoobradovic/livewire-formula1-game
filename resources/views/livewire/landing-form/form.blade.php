ae4b05ff5d60f1de5f39914ae18e136a459c72d3
Zivko <zozo@Zivkos-MacBook-Pro.local>
2023-02-19 13:34:10 +0100

    Arabic corrected

--- resources/views/livewire/landing-form/form.blade.php
+++ resources/views/livewire/landing-form/form.blade.php
@@ -12,7 +12,7 @@
     <input id="default-radio-2" type="radio" value="arab" name="" wire:click="changeLanguage" {{$language === 'arab' ? 'checked' : ''}}
         wire:model="language"
         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-    <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Arabian</label>
+    <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Arabic</label>
 </div>
             <!-- Nav tabs -->
             <ul class="nav nav-pills nav-justified" role="tablist">
