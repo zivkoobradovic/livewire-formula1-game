c966fb26d3312c2954e23883d0c47b5e0d417a3c
Mladen Sreckovic <mladen.sreckovic.10@gmail.com>
2023-02-23 10:14:22 +0100

    Title updated

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'F1') }}</title>
+    <title>{{ config('app.name', 'Saudi GP') }}</title>
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
 
