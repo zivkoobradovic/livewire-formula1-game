75cab01b1ee5b6eabee9f398d987611ae92b6f18
Mladen Sreckovic <mladen.sreckovic.10@gmail.com>
2023-02-23 17:45:32 +0100

    fixed jquery iphone

--- public/game/js/home.js
+++ public/game/js/home.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
     $('.loader').delay(1000).fadeOut(function() {
 
     });
-    $('form input').on('keyup', function(){
+    $('.email-input').on('keyup touchend input change', function(){
       $(this).val($(this).val().toLowerCase());
     });
-
+    setTimeout(function() {
+    $('.email-input').val($('.email-input').val().toLowerCase());
+  }, 500);
 
 });
