6d3b31d5ef2fb0d6d726da4ae653a86604a85bd8
Mladen Sreckovic <mladen.sreckovic.10@gmail.com>
2023-02-23 17:40:16 +0100

    fixed keyup event

--- public/game/js/home.js
+++ public/game/js/home.js
@@ -3,11 +3,9 @@ $(document).ready(function() {
     $('.loader').delay(1000).fadeOut(function() {
 
     });
-    $('form input').on('keyup input', function(){
+    $('form input').on('keyup', function(){
       $(this).val($(this).val().toLowerCase());
     });
-    $('form input').on('keyup touchend', function(){
-      $(this).val($(this).val().toLowerCase());
-    });
+
 
 });
