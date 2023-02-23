30214ca6bce607b5eba029c616dbc71726486556
Mladen Sreckovic <mladen.sreckovic.10@gmail.com>
2023-02-23 17:32:10 +0100

    fixed email validation

--- public/game/js/home.js
+++ public/game/js/home.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.loader').delay(1000).fadeOut(function() {
 
     });
-    $('form input').keyup(function() {
+    $('form input').on('keyup input', function(){
       $(this).val($(this).val().toLowerCase());
     });
 
