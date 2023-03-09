af94e7e6d087d1ec54c757e82d853b1d44e0e6a9
Zivko <zozo@Zivkos-MacBook-Pro.local>
2023-03-09 11:03:11 +0100

    dashboard logo added

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,10 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        {{-- <x-jet-application-mark class="block h-9 w-auto" /> --}}
+
+                            <img class="w-20" src="{{asset('game/img/logo.svg')}}" />
+
                     </a>
                 </div>
 
